8ff6e89bafaa138c1fd6b5890034c856973b8d29
JackTench <79285604+JackTench@users.noreply.github.com>
2024-06-11 20:57:42 +0100

    Basic setup of cli parser.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct CliArgs {
+    command: String,
+    game: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = CliArgs::parse();
+
+    if args.command == "run".to_string() {
+        println!("running {}", args.game)
+    }
 }
