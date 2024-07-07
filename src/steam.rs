f62925d712fb8c316cffbf7730c6b58d2a071549
JackTench <79285604+JackTench@users.noreply.github.com>
2024-07-07 17:02:06 +0100

    Parse JSON to extract game name and steam app id.

--- src/steam.rs
+++ src/steam.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 
 use dotenv::dotenv;
 use reqwest::blocking::Client;
+use serde_json::Value;
 
 pub fn import_steam_games(steam_id: &str) -> Result<(), Box<dyn Error>> {
     // Load Steam API key environment variable.
@@ -17,6 +18,14 @@ pub fn import_steam_games(steam_id: &str) -> Result<(), Box<dyn Error>> {
     );
 
     let response = client.get(&query_url).send()?.text()?;
+    let json: Value = serde_json::from_str(&response)?;
+
+    if let Some(games) = json.pointer("/response/games").and_then(|v| v.as_array()) {
+        for game in games {
+            let appid = game.get("appid").and_then(|v| v.as_i64()).unwrap_or(-1) as i32;
+            let name = game.get("name").and_then(|v| v.as_str()).unwrap_or("Unknown Game").to_string();
+        }
+    }
 
     Ok(())
 }
