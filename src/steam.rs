c4386f79d0563810a1dcb29dcf2918274e5de6fc
JackTench <79285604+JackTench@users.noreply.github.com>
2024-07-07 16:52:55 +0100

    Load API key for environment variables.

--- src/steam.rs
+++ src/steam.rs
@@ -1,3 +1,9 @@
-pub fn import_steam_games() {
+use std::env;
 
+use dotenv::dotenv;
+
+pub fn import_steam_games() {
+    // Load Steam API key environment variable.
+    dotenv().ok();
+    let api_key = env::var("STEAM_API_KEY").expect("Could not find Steam API key environment variable.");
 }
