c1026a46667cda987d57e77bb56409edb7adabcb
JackTench <79285604+JackTench@users.noreply.github.com>
2024-10-04 14:11:06 +0100

    Parse response back from Steam API.

--- src/steamapi.rs
+++ src/steamapi.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 
 use reqwest::blocking::Client;
+use serde_json::Value;
 
 pub fn import_steam_games(steam_api_key: &str, steam_id: &str) -> Result<(), Box<dyn Error>> {
     // Download JSON of steam games for given SteamID.
@@ -10,7 +11,17 @@ pub fn import_steam_games(steam_api_key: &str, steam_id: &str) -> Result<(), Box
         steam_api_key, steam_id
     );
     let response = client.get(&query_url).send()?.text()?;
-    println!("{}", response);
+    //println!("{}", response);
+
+    // Parse JSON response.
+    let json: Value = serde_json::from_str(&response).unwrap();
+    if let Some(games) = json.pointer("/response/games").and_then(|v| v.as_array()) {
+        for game in games {
+            let name = game.get("name").unwrap();
+            let steam_app_id = game.get("appid").unwrap().to_string();
+            println!("{} {}", name, steam_app_id);
+        }
+    }
 
     Ok(())
 }
