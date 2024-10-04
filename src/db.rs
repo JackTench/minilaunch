89096282798b438ef112ecdd9ebcab9e016e9791
JackTench <79285604+JackTench@users.noreply.github.com>
2024-10-04 13:29:02 +0100

    Create SQL schema.

--- src/db.rs
+++ src/db.rs
@@ -2,5 +2,7 @@ use rusqlite::Connection;
 
 pub fn setup_database() {
     let conn = Connection::open("minilaunchdata.db").unwrap();
+    // Set database schema if first time launch.
+    conn.execute_batch(include_str!("sql/schema.sql")).unwrap();
     let _ = conn.close();
 }
