85ff8dd3350ec9b8d8198dcd26ef0795a03973b0
JackTench <79285604+JackTench@users.noreply.github.com>
2024-10-04 14:52:41 +0100

    Fetch games from Database.
    Implements some basic ORM-style thing.

--- src/db.rs
+++ src/db.rs
@@ -3,6 +3,11 @@ use rusqlite::{
     params
 };
 
+pub struct Game {
+    name: String,
+    steamappid: i64
+}
+
 pub fn setup_database() {
     let conn = Connection::open("minilaunchdata.db").unwrap();
     // Set database schema if first time launch.
@@ -14,4 +19,26 @@ pub fn add_game(name: String, steamappid: i64) {
     let conn = Connection::open("minilaunchdata.db").unwrap();
     conn.execute(include_str!("sql/add_game.sql"), params![name, steamappid]).unwrap();
     let _ = conn.close();
+}
+
+pub fn get_games() -> Vec<Game> {
+    let conn = Connection::open("minilaunchdata.db").unwrap();
+
+    // Query database.
+    let mut statement = conn.prepare(include_str!("sql/get_games.sql")).map_err(|e| e.to_string()).unwrap();
+
+    // Fetch rows.
+    let games_iter = statement.query_map(params![], |row| {
+        Ok(Game {
+            name: row.get(0)?,
+            steamappid: row.get(1)?,
+        })
+    }).unwrap();
+
+    // Collect into vector.
+    let games: Vec<Game> = games_iter
+        .map(|game| game.unwrap())
+        .collect();
+
+    games
 }
---
+++ src/sql/get_games.sql
@@ -0,0 +1,1 @@
+SELECT * FROM games ORDER BY LOWER(name);
