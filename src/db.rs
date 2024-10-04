aaa931812ac6ae29abc850a8d082ce7f2db8b3fe
JackTench <79285604+JackTench@users.noreply.github.com>
2024-10-04 13:35:35 +0100

    Implement adding games to database.

--- src/db.rs
+++ src/db.rs
@@ -1,8 +1,17 @@
-use rusqlite::Connection;
+use rusqlite::{
+    Connection,
+    params
+};
 
 pub fn setup_database() {
     let conn = Connection::open("minilaunchdata.db").unwrap();
     // Set database schema if first time launch.
     conn.execute_batch(include_str!("sql/schema.sql")).unwrap();
+    let _ = conn.close();
+}
+
+pub fn add_game(name: String, steamappid: i64) {
+    let conn = Connection::open("minilaunchdata.db").unwrap();
+    conn.execute(include_str!("sql/add_game.sql"), params![name, steamappid]).unwrap();
     let _ = conn.close();
 }
