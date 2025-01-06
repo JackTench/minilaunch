12760c4b52505a2540bf65af3b7a79cc91784064
JackTench <79285604+JackTench@users.noreply.github.com>
2025-01-06 21:02:09 +0000

    Create a more friendly database schema for non-steam and emulator games.

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -1,6 +1,15 @@
+CREATE TABLE IF NOT EXISTS platforms (
+	id INTEGER PRIMARY KEY,
+	name TEXT NOT NULL,
+	launch_prefix TEXT NOT NULL,
+	CONSTRAINT unique_platform UNIQUE (launch_prefix)
+);
+
 CREATE TABLE IF NOT EXISTS games (
 	id INTEGER PRIMARY KEY,
 	name TEXT NOT NULL,
-	steamappid INTEGER NOT NULL,
-	CONSTRAINT unique_game UNIQUE (steamappid)
+	platform_id INTEGER NOT NULL,
+	launch_postfix TEXT NOT NULL,
+	CONSTRAINT unique_game UNIQUE (platform_id, launch_postfix),
+	FOREIGN KEY (platform_id) REFERENCES platforms (id)
 );
